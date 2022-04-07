ae291b7b5261e8ab9b586ba577016b686afa9dbe
Wassim Khalil <wassimkhalil810@gmail.com>
2022-04-07 22:11:26 +0300

    Initial Commit

--- routes/orders.php
+++ routes/orders.php
@@ -7,6 +7,7 @@ use Slim\Factory\AppFactory;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
+use GuzzleHttp\Client;
 
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
@@ -159,31 +160,59 @@ $app->delete('/api/offline/orders/{id}', function (Request $request, Response $r
 // synchronize databases
 $app->get('/api/sync', function (Request $request, Response $response) {
     try {
+        $errors = 0;
+        $i = 0;
+        $u = 0;
         $db = new DB();
         $conn = $db->connect();
         $conn2 = $db->connect2();
+        //rows to update and insert
         $sql = "SELECT c.*, CASE
         WHEN c.id NOT IN ( select id from `online-db`.orders ) THEN 1
         WHEN c.id IN ( select id from `online-db`.orders ) THEN 2
         END AS Test
-        FROM  `not-online`.orders c  where sync = 0;";
+        FROM  `not-online`.orders c where sync = 0;";
         $stmt = $conn->query($sql);
         $orders = $stmt->fetchAll();
-        foreach ($orders as $row) {
-            if($row['Test']=='1'){
-                $conn2->query("Insert into orders values ('" . $row['id'] . "', '" . $row['flavor'] . "', '" . $row['quantity'] . "', '" . $row['orderto'] . "', '" . $row['orderdate'] . "', '1')");
-                $conn->query("Update orders set sync = 1 where id =  '" . $row['id'] . "'");
-            }else if($row['Test']=='2'){
-                $conn2->query("Update orders set flavor = '" . $row['flavor'] . "', quantity = '" . $row['quantity'] . "', orderto = '" . $row['orderto'] . "', orderdate = '" . $row['orderdate'] . "', sync=  '1' where id = '" . $row['id'] . "';");
-                $conn->query("Update orders set sync = 1 where id =  '" . $row['id'] . "'");
-
+        $count = $stmt->rowCount();
+        //rows to delete
+        $del = $conn->prepare('delete from `online-db`.orders where id not in (select id from `not-online`.orders);');
+        $del->execute();
+        $d = $del->rowCount();
+        if ( $count > 0 || $d >0) {
+            foreach ($orders as $row) {
+                if ($row['Test'] == '1') {
+                    $insert1 = "Insert into orders values ('" . $row['id'] . "', '" . $row['flavor'] . "', '" . $row['quantity'] . "', '" . $row['orderto'] . "', '" . $row['orderdate'] . "', '1')";
+                    $update1 = "Update orders set sync = 1 where id =  '" . $row['id'] . "'";
+                    if ($conn2->query($insert1) && $conn->query($update1)) {
+                        // echo 'Order to'. $row['orderto'] .'has been synched           |       ';
+                        $i++;
+                    } else {
+                        $errors++;
+                    }
+                } else if ($row['Test'] == '2') {
+                    $update2 = "Update orders set flavor = '" . $row['flavor'] . "', quantity = '" . $row['quantity'] . "', orderto = '" . $row['orderto'] . "', orderdate = '" . $row['orderdate'] . "', sync=  '1' where id = '" . $row['id'] . "';";
+                    $update3 = "Update orders set sync = 1 where id =  '" . $row['id'] . "'";
+                    if ($conn2->query($update2) && $conn->query($update3)) {
+                        // echo 'Order to '. $row['orderto'] .' has been Updated     |     ';
+                        $u++;
+                    } else {
+                        $errors++;
+                    }
+                }
             }
+            //$conn2->query('delete from `online-db`.orders where id not in (select id from `not-online`.orders);');
+
+            $result = 'Synchronization Successfull with ' . $errors . ' errors! ' . $i . ' row(s) inserted, ' . $u . ' row(s) updated and ' . $d . ' row(s) deleted. An email will be sent to the admin.';
+            //echo $errors.' errors';
+            sendVerificationEmail('abuelwiss8@gmail.com', $errors, $i, $u, $d);
+            $response->getBody()->write(json_encode($result));
+            return $response->withHeader('content-type', 'application/json')->withStatus(200);
+        } else {
+            $result = "Nothing To synchronize";
+            $response->getBody()->write(json_encode($result));
+            return $response->withHeader('content-type', 'application/json')->withStatus(200);
         }
-        $conn2->query('delete from `online-db`.orders where id not in (select id from `not-online`.orders);');
-        $result = 'Synchronization Successfull ! An email will be sent to the admin.';
-        sendVerificationEmail('abuelwiss8@gmail.com');
-        $response->getBody()->write(json_encode($result));
-        return $response->withHeader('content-type', 'application/json')->withStatus(200);
     } catch (PDOException $e) {
         $error = array("message" => $e->getMessage());
         $response->getBody()->write(json_encode($error));
@@ -191,6 +220,18 @@ $app->get('/api/sync', function (Request $request, Response $response) {
     }
 });
 
+$app->get('/api/sync2', function (Request $request, Response $response) {
+    $client = new Client([
+        // Base URI is used with relative requests
+        'base_uri' => 'http://localhost:8000',
+        'defaults' => [
+            'exceptions' => false
+        ]
+    ]);
+
+    $response = $client->request('GET', '/api/synchronize');
+    return $response;
+});
 //fetch all online orders
 $app->get('/api/online/orders/all', function (Request $request, Response $response) {
     $sql = "Select * from orders";
@@ -209,35 +250,42 @@ $app->get('/api/online/orders/all', function (Request $request, Response $respon
     }
 });
 
+$app->get('/api/online/orders/all2', function (Request $request, Response $response) {
+    $client = new Client([
+        // Base URI is used with relative requests
+        'base_uri' => 'http://localhost:8000',
+        'defaults' => [
+            'exceptions' => false
+        ]
+    ]);
 
-function sendVerificationEmail($email)
-{      
+    $response = $client->request('GET', '/api/online/orders/all');
+    return $response;
+});
+
+
+function sendVerificationEmail($email, $errors, $i, $u, $d)
+{
     $mail = new PHPMailer;
     //$mail->SMTPDebug=3;
     $mail->isSMTP();
 
-    $mail->Host="smtp.gmail.com";
-    $mail->Port=587;
-    $mail->SMTPSecure="tls";
-    $mail->SMTPAuth=true;
-    $mail->Username="wassimorders@gmail.com";
-    $mail->Password="P@ssw0rd961";
-
-    $mail->addAddress($email,"User Name");
-    $mail->Subject="Synchronization Successfull!";
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 587;
+    $mail->SMTPSecure = "tls";
+    $mail->SMTPAuth = true;
+    $mail->Username = "wassimorders@gmail.com";
+    $mail->Password = "P@ssw0rd961";
+    $mail->addAddress($email, "User Name");
+    $mail->Subject = "Synchronization Successfull with " . $errors . " errors !";
     $mail->isHTML();
-    $mail->Body="You have successfully synchronized data from the offline database to the online one.";
-    $mail->From="flagm@gmail.com";
-    $mail->FromName="Flag M";
+    $mail->Body = "You have successfully synchronized data from the offline database to the online one.<br>Number of rows inserted: " . $i . "<br>Number of rows updated: " . $u . "<br>Number of rows deleted: " . $d . "";
+    $mail->From = "flagm@gmail.com";
+    $mail->FromName = "Flag M";
 
-    if($mail->send())
-    {
-       // echo "Email Has Been Sent Your Email Address";
-    }
-    else
-    {
+    if ($mail->send()) {
+        // echo "Email Has Been Sent Your Email Address";
+    } else {
         echo "Failed To Sent An Email To Your Email Address";
     }
-
-
 }
